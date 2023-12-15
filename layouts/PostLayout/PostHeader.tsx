5fd94c570f5445792c5c23075eb3a5a5c3fa2f51
Justin <57725347+juskek@users.noreply.github.com>
2023-12-15 16:37:31 +0000

    refactor: PostHeader

---
+++ layouts/PostLayout/PostHeader.tsx
@@ -0,0 +1,35 @@
+import PageTitle from "@/components/PageTitle"
+
+const postDateTemplate: Intl.DateTimeFormatOptions = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  }
+  
+type Props = {
+    title: string
+    date: string
+    locale:string
+    
+}
+
+export const PostHeader = ({title, date, locale }:Props) => {
+    return ( <header className="pt-6 xl:pb-6">
+    <div className="space-y-1 text-center">
+      <dl className="space-y-10">
+        <div>
+          <dt className="sr-only">Published on</dt>
+          <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
+            <time dateTime={date}>
+              {new Date(date).toLocaleDateString(locale, postDateTemplate)}
+            </time>
+          </dd>
+        </div>
+      </dl>
+      <div>
+        <PageTitle>{title}</PageTitle>
+      </div>
+    </div>
+  </header>)
+}
--- layouts/PostLayout/PostLayout.tsx
+++ layouts/PostLayout/PostLayout.tsx
@@ -9,6 +9,7 @@ import Image from '@/components/Image'
 import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import ScrollTopAndComment from '@/components/ScrollTopAndComment'
+import { PostHeader } from './PostHeader'
 
 const editUrl = (path) => `${siteMetadata.siteRepo}/blob/main/data/${path}`
 const discussUrl = (path) =>
@@ -38,23 +39,7 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
       <ScrollTopAndComment />
       <article>
         <div className="xl:divide-y xl:divide-gray-200 xl:dark:divide-gray-700">
-          <header className="pt-6 xl:pb-6">
-            <div className="space-y-1 text-center">
-              <dl className="space-y-10">
-                <div>
-                  <dt className="sr-only">Published on</dt>
-                  <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
-                    <time dateTime={date}>
-                      {new Date(date).toLocaleDateString(siteMetadata.locale, postDateTemplate)}
-                    </time>
-                  </dd>
-                </div>
-              </dl>
-              <div>
-                <PageTitle>{title}</PageTitle>
-              </div>
-            </div>
-          </header>
+          <PostHeader title={title} date={date} locale={siteMetadata.locale} />
           <div className="grid-rows-[auto_1fr] divide-y divide-gray-200 pb-8 dark:divide-gray-700 xl:grid xl:grid-cols-4 xl:gap-x-6 xl:divide-y-0">
             <dl className="pb-10 pt-6 xl:border-b xl:border-gray-200 xl:pt-11 xl:dark:border-gray-700">
               <dt className="sr-only">Authors</dt>
