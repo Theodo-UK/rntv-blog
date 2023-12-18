c024703436702476cd3b6bfa2dd70c799b8f1597
Justin <57725347+juskek@users.noreply.github.com>
2023-12-18 22:56:29 +0800

    refactor: create generateArticleMetadata

--- app/blog/[...slug]/page.tsx
+++ app/blog/[...slug]/page.tsx
@@ -2,7 +2,6 @@ import 'css/prism.css'
 import 'katex/dist/katex.css'
 
 import { components } from '@/components/MDXComponents'
-import siteMetadata from '@/data/siteMetadata'
 import PostBanner from '@/layouts/PostBanner'
 import PostLayout from '@/layouts/PostLayout/PostLayout'
 import PostSimple from '@/layouts/PostSimple'
@@ -25,6 +24,7 @@ import fs from 'fs'
 import matter from 'gray-matter'
 import path from 'path'
 import { remark } from 'remark'
+import { generateArticleMetadata } from './generateArticleMetadata'
 
 const postsDirectory = path.join(process.cwd(), 'data/blog')
 
@@ -46,52 +46,7 @@ export async function generateMetadata({
 }: {
   params: { slug: string[] }
 }): Promise<Metadata | undefined> {
-  const slug = decodeURI(params.slug.join('/'))
-  const post = allBlogs.find((p) => p.slug === slug)
-  const authorList = post?.authors || ['default']
-  const authorDetails = authorList.map((author) => {
-    const authorResults = allAuthors.find((p) => p.slug === author)
-    return coreContent(authorResults as Authors)
-  })
-  if (!post) {
-    return
-  }
-
-  const publishedAt = new Date(post.date).toISOString()
-  const modifiedAt = new Date(post.lastmod || post.date).toISOString()
-  const authors = authorDetails.map((author) => author.name)
-  let imageList = [siteMetadata.socialBanner]
-  if (post.images) {
-    imageList = typeof post.images === 'string' ? [post.images] : post.images
-  }
-  const ogImages = imageList.map((img) => {
-    return {
-      url: img.includes('http') ? img : siteMetadata.siteUrl + img,
-    }
-  })
-
-  return {
-    title: post.title,
-    description: post.summary,
-    openGraph: {
-      title: post.title,
-      description: post.summary,
-      siteName: siteMetadata.title,
-      locale: 'en_US',
-      type: 'article',
-      publishedTime: publishedAt,
-      modifiedTime: modifiedAt,
-      url: './',
-      images: ogImages,
-      authors: authors.length > 0 ? authors : [siteMetadata.author],
-    },
-    twitter: {
-      card: 'summary_large_image',
-      title: post.title,
-      description: post.summary,
-      images: imageList,
-    },
-  }
+  return generateArticleMetadata(params.slug)
 }
 
 export const generateStaticParams = async () => {
