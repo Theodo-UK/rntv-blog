7cf6efe2a52b9ddc23002fd151a3bf1755a410c8
Justin <57725347+juskek@users.noreply.github.com>
2023-12-23 00:42:43 +0800

    feat: create Headline

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -3,7 +3,7 @@ const siteMetadata = {
   title: 'Next.js Starter Blog',
   author: 'Tails Azimuth',
   headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  description: 'Building Cutting-Edge TV Apps with React Native',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
