0390d73c87544d48863ca8153a6ad1f925bb6890
Justin <57725347+juskek@users.noreply.github.com>
2023-12-27 15:41:12 +0000

    bug: keywords for searching cut off after 'five'

---
+++ src/features/search/SearchProvider.tsx
@@ -0,0 +1,62 @@
+'use client'
+import { Blog } from 'contentlayer/generated'
+import { useRouter } from 'next/navigation'
+import { KBarSearchProvider } from 'pliny/search/KBar'
+import { CoreContent } from 'pliny/utils/contentlayer'
+
+export const SearchProvider = ({ children }) => {
+  const router = useRouter()
+  return (
+    <KBarSearchProvider
+      kbarConfig={{
+        searchDocumentsPath: 'search.json',
+        // defaultActions: [
+        //   {
+        //     id: 'homepage',
+        //     name: 'Homepage',
+        //     keywords: '',
+        //     shortcut: ['h', 'h'],
+        //     section: 'Home',
+        //     perform: () => router.push('/'),
+        //   },
+        //   {
+        //     id: 'projects',
+        //     name: 'Projects',
+        //     keywords: '',
+        //     shortcut: ['p'],
+        //     section: 'Home',
+        //     perform: () => router.push('/projects'),
+        //   },
+        // ],
+        onSearchDocumentsLoad(json) {
+          return json.map((post: CoreContent<Blog>) => {
+            const test = `sss ${post?.summary ?? ''} ${post?.tags?.join(' ') ?? ''} ${
+              post.date ?? ''
+            } ${post.authors?.join(' ') ?? ''}`
+            console.log(test)
+
+            return {
+              id: post.path,
+              name: 'hi',
+              keywords: 'one two three four five six seven',
+              subtitle: `${post.date} | ${post.authors?.join(', ')} | ${post.tags.join(', ')}`,
+              perform: () => router.push(post.path),
+              //   name: post.title,
+              //   keywords: `${post.authors?.join(' ') ?? ''}}`,
+              //   keywords: 'ab cd ef gh ij kl mn op qr st uv wx yz',
+              //   keywords: `sss ${post?.summary ?? ''} ${post?.tags?.join(' ') ?? ''} ${
+              //     post.date ?? ''
+              //   } ${post.authors?.join(' ') ?? ''}`,
+              //   section: 'I am a section',
+              // shortcut: ['i', 'am', 'a', 'shortcut'],
+              // icon: 'https://www.google.com/favicon.ico',
+              // priority: 1,
+            }
+          })
+        },
+      }}
+    >
+      {children}
+    </KBarSearchProvider>
+  )
+}
