f0e34ce4e466a929f8799a8006c246bf08a02b46
Justin <57725347+juskek@users.noreply.github.com>
2023-12-28 09:57:49 +0000

    feat: load comments automatically

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,11 +1,11 @@
 'use client'
 
+import siteMetadata from '@/data/siteMetadata'
 import { Comments as CommentsComponent } from 'pliny/comments'
 import { useState } from 'react'
-import siteMetadata from '@/data/siteMetadata'
 
 export default function Comments({ slug }: { slug: string }) {
-  const [loadComments, setLoadComments] = useState(false)
+  const [loadComments, setLoadComments] = useState(true)
   return (
     <>
       {!loadComments && <button onClick={() => setLoadComments(true)}>Load Comments</button>}
