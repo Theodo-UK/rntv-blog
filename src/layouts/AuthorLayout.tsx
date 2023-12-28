aba66b1e8a414fcd09b9d8ff92a4d28fd67a79fd
Justin <57725347+juskek@users.noreply.github.com>
2023-12-28 09:50:24 +0000

    feat: multiple team members on about page

--- src/layouts/AuthorLayout.tsx
+++ src/layouts/AuthorLayout.tsx
@@ -14,11 +14,6 @@ export default function AuthorLayout({ children, content }: Props) {
   return (
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
-        <div className="space-y-2 pb-8 pt-6 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            About
-          </h1>
-        </div>
         <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
           <div className="flex flex-col items-center space-x-2 pt-8">
             {avatar && (
