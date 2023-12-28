78cd2b43e607a26ab70b7a42b193ef643df454e0
Justin <57725347+juskek@users.noreply.github.com>
2023-12-28 11:36:59 +0000

    chore: remove social icons in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,19 +1,18 @@
 import siteMetadata from '@/data/siteMetadata'
-import SocialIcon from 'src/components/social-icons'
 import Link from './Link'
 
 export default function Footer() {
   return (
     <footer>
       <div className="mt-16 flex flex-col items-center">
-        <div className="mb-3 flex space-x-4">
+        {/* <div className="mb-3 flex space-x-4">
           <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size={6} />
           <SocialIcon kind="github" href={siteMetadata.github} size={6} />
           <SocialIcon kind="facebook" href={siteMetadata.facebook} size={6} />
           <SocialIcon kind="youtube" href={siteMetadata.youtube} size={6} />
           <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size={6} />
           <SocialIcon kind="twitter" href={siteMetadata.twitter} size={6} />
-        </div>
+        </div> */}
         <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
           <Link href="/">{siteMetadata.title}</Link>
           <div>{` • `}</div>
