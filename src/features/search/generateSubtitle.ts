794c15f8ff1af9d7eb60613889e388f8476bcd5a
Justin <57725347+juskek@users.noreply.github.com>
2023-12-28 16:33:19 +0000

    fix: search author name not slug

--- src/features/search/generateSubtitle.ts
+++ src/features/search/generateSubtitle.ts
@@ -1,5 +1,6 @@
-import { Blog, allAuthors } from 'contentlayer/generated'
+import { Blog } from 'contentlayer/generated'
 import { CoreContent } from 'pliny/utils/contentlayer'
+import { generateAuthorNames } from './generateAuthorNames'
 
 export function generateSubtitle(post: CoreContent<Blog>) {
   const dateSubtitle = new Date(post.date).toLocaleDateString('en-UK', {
@@ -7,16 +8,7 @@ export function generateSubtitle(post: CoreContent<Blog>) {
     month: 'short',
     day: 'numeric',
   })
-  const authorNames: string[] = []
-  if (post.authors !== undefined) {
-    for (const author of post.authors) {
-      const authorResults = allAuthors.find((p) => p.slug === author)
-      if (authorResults === undefined) {
-        continue
-      }
-      authorNames.push(authorResults.name)
-    }
-  }
+  const authorNames = generateAuthorNames(post)
   const authorsSubtitle = post.authors === undefined ? '' : ` · ${authorNames.join(', ')}`
   const tagsSubtitle = post.tags === undefined ? '' : ` · ${post.tags.join(', ')}`
   const subtitle = `${dateSubtitle}${authorsSubtitle}${tagsSubtitle}`
