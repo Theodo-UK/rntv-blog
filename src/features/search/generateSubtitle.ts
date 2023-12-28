f3a6817a0573e0b4ea4eacb30adcf448d08c7d3a
Justin <57725347+juskek@users.noreply.github.com>
2023-12-28 16:25:48 +0000

    feat: show author name in kbar results

--- src/features/search/generateSubtitle.ts
+++ src/features/search/generateSubtitle.ts
@@ -1,4 +1,4 @@
-import { Blog } from 'contentlayer/generated'
+import { Blog, allAuthors } from 'contentlayer/generated'
 import { CoreContent } from 'pliny/utils/contentlayer'
 
 export function generateSubtitle(post: CoreContent<Blog>) {
@@ -7,7 +7,17 @@ export function generateSubtitle(post: CoreContent<Blog>) {
     month: 'short',
     day: 'numeric',
   })
-  const authorsSubtitle = post.authors === undefined ? '' : ` · ${post.authors.join(', ')}`
+  const authorNames: string[] = []
+  if (post.authors !== undefined) {
+    for (const author of post.authors) {
+      const authorResults = allAuthors.find((p) => p.slug === author)
+      if (authorResults === undefined) {
+        continue
+      }
+      authorNames.push(authorResults.name)
+    }
+  }
+  const authorsSubtitle = post.authors === undefined ? '' : ` · ${authorNames.join(', ')}`
   const tagsSubtitle = post.tags === undefined ? '' : ` · ${post.tags.join(', ')}`
   const subtitle = `${dateSubtitle}${authorsSubtitle}${tagsSubtitle}`
   return subtitle
