b48d05957166368e4d5c720237cb9b079557b3a1
Justin <57725347+juskek@users.noreply.github.com>
2023-12-28 11:12:37 +0000

    feat: make colours consistent

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,7 @@ module.exports = {
         sans: ['var(--font-space-grotesk)', ...fontFamily.sans],
       },
       colors: {
-        primary: colors.pink,
+        primary: colors.orange,
         gray: colors.gray,
       },
       typography: ({ theme }) => ({
