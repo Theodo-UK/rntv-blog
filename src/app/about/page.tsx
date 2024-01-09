ea59976d9169591be2990cb475dd2f72f80e8c83
Justin <57725347+juskek@users.noreply.github.com>
2024-01-09 12:53:12 +0000

    feat: update Our Goal on about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -15,24 +15,30 @@ export default function Page() {
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Our Vision
+            Our Goal
           </h1>
         </div>
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
           <p>
-            Our vision at ReactNativeTV.com is to be the premier destination for everything React
-            Native in the world of television. We aim to centralise and simplify learning, offering
-            an extensive repository of resources, tutorials, and insights. Our goal is to empower
-            developers of all levels to harness the full potential of React Native in creating
-            innovative TV applications, bridging the gap between traditional TV experiences and
-            cutting-edge technology.
+            Our goal is to create a consolidative knowledge base for developers who are interested
+            in venturing into the realm of building TV apps, particularly those based on React
+            Native (RN).
+          </p>
+          <p>
+            Currently, there exists a noticeable gap in readily available information in this
+            specialised area, making it an underloved part of the RN ecosystem.
+          </p>
+          <p>
+            We recognise this lack and aim to fill it by establishing a centralised hub where people
+            can not only find comprehensive resources and guidance but also feel like they are part
+            of a community dedicated to learning and growing in this unique space.
           </p>
         </div>
       </div>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            About
+            Editors
           </h1>
         </div>
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
