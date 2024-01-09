622b03c61a79d9c43fd4c9676d6e183578e475af
Justin <57725347+juskek@users.noreply.github.com>
2024-01-09 14:07:29 +0000

    refactor: create OurGoal

---
+++ src/features/AboutPage/OurGoal.tsx
@@ -0,0 +1,27 @@
+export const OurGoal = () => {
+  return (
+    <div className="divide-y divide-gray-200 dark:divide-gray-700">
+      <div className="space-y-2 pb-8 pt-6 md:space-y-5">
+        <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+          Our Goal
+        </h1>
+      </div>
+      <div className="space-y-2 pb-8 pt-6 md:space-y-5">
+        <p>
+          Our goal is to create a consolidative knowledge base for developers who are interested in
+          venturing into the realm of building TV apps, particularly those based on React Native
+          (RN).
+        </p>
+        <p>
+          Currently, there exists a noticeable gap in readily available information in this
+          specialised area, making it an underloved part of the RN ecosystem.
+        </p>
+        <p>
+          We recognise this lack and aim to fill it by establishing a centralised hub where people
+          can not only find comprehensive resources and guidance but also feel like they are part of
+          a community dedicated to learning and growing in this unique space.
+        </p>
+      </div>
+    </div>
+  )
+}
