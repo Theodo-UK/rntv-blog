3fbc909fa7e5c316e1ad0138007f48d05340c40a
Justin <57725347+juskek@users.noreply.github.com>
2024-01-09 14:04:36 +0000

    feat: change colour of code in article

--- tailwind.config.js
+++ tailwind.config.js
@@ -39,7 +39,7 @@ module.exports = {
               fontWeight: '600',
             },
             code: {
-              color: theme('colors.indigo.500'),
+              color: theme('colors.primary.400'),
             },
           },
         },
