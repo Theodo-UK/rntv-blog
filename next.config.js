ff7404383f403b75671d4ce961115be2dce109cd
Justin <57725347+juskek@users.noreply.github.com>
2024-01-09 16:25:51 +0000

    feat: use youtube thumbnail for videos page

--- next.config.js
+++ next.config.js
@@ -71,6 +71,10 @@ module.exports = () => {
           protocol: 'https',
           hostname: 'picsum.photos',
         },
+        {
+          protocol: 'http',
+          hostname: 'img.youtube.com',
+        },
       ],
     },
     async headers() {
