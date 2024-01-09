4e58d713523fb6cbcd61fea5e24c98d503de1446
Justin <57725347+juskek@users.noreply.github.com>
2024-01-09 15:51:46 +0000

    fix: remove additional white space on HomePage on small screen sizes

--- src/features/HomePage/Headline.tsx
+++ src/features/HomePage/Headline.tsx
@@ -3,9 +3,16 @@ import Image from 'next/image'
 
 export const Headline = () => {
   return (
-    <div className="space-y-2 pb-8 pt-6 md:space-y-5">
-      <div className="grid grid-cols-1 grid-rows-2 justify-items-end gap-2 sm:grid-cols-2 sm:grid-rows-1">
-        <div className="row-start-2 sm:col-start-1 sm:row-start-1">
+    <div className="space-y-2 pb-4 pt-6 md:space-y-5">
+      <div className="grid grid-cols-1 justify-items-end gap-4 sm:grid-cols-2 sm:grid-rows-1 ">
+        <Image
+          className="row-start-1 rounded sm:col-start-2"
+          src="/static/images/react-native-tv.webp"
+          alt="React Native TV"
+          width={500}
+          height={500}
+        />
+        <div className="sm:col-start-1 sm:row-start-1">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-3xl sm:leading-10 md:text-5xl md:leading-14">
             Bringing React to the Big Screen
           </h1>
@@ -13,13 +20,6 @@ export const Headline = () => {
             {siteMetadata.description}
           </p>
         </div>
-        <Image
-          className="row-start-1 rounded sm:col-start-2 sm:row-start-1"
-          src="/static/images/react-native-tv.webp"
-          alt="React Native TV"
-          width={500}
-          height={500}
-        />
       </div>
     </div>
   )
