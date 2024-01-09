cdb0188ed2abf62d81fab60595f2950a6eb81435
Justin <57725347+juskek@users.noreply.github.com>
2024-01-09 15:23:31 +0000

    fix: author layout issues

--- src/layouts/PostLayout/PostAuthors/PostAuthors.tsx
+++ src/layouts/PostLayout/PostAuthors/PostAuthors.tsx
@@ -11,9 +11,9 @@ export const PostAuthors = ({ authorDetails }: Props) => {
     <dl className="pb-10 pt-6 xl:border-b xl:border-gray-200 xl:pt-11 xl:dark:border-gray-700">
       <dt className="sr-only">Authors</dt>
       <dd>
-        <ul className="flex flex-wrap justify-center gap-4 sm:space-x-12 xl:block xl:space-x-0 xl:space-y-8">
+        <ul className="items-left flex flex-col gap-4">
           {authorDetails.map((author) => (
-            <li className="flex items-center space-x-2" key={author.name}>
+            <li className="justify-content flex items-center gap-2" key={author.name}>
               {author.avatar && (
                 <Image
                   src={author.avatar}
