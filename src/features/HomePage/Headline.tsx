9d40b5f8582d78c8aaef4b70311e14a65df4e3f4
Justin <57725347+juskek@users.noreply.github.com>
2024-01-09 15:37:35 +0000

    fix: align HomePage image with right side

--- src/features/HomePage/Headline.tsx
+++ src/features/HomePage/Headline.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export const Headline = () => {
   return (
     <div className="space-y-2 pb-8 pt-6 md:space-y-5">
-      <div className="grid grid-cols-1 grid-rows-2 sm:grid-cols-2 sm:grid-rows-1">
+      <div className="grid grid-cols-1 grid-rows-2 justify-items-end gap-2 sm:grid-cols-2 sm:grid-rows-1">
         <div className="row-start-2 sm:col-start-1 sm:row-start-1">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-3xl sm:leading-10 md:text-5xl md:leading-14">
             Bringing React to the Big Screen
