a6e43b61accf937f5615d3ff44ce206e70f02d4b
Mo Javad <34189022+mojavad@users.noreply.github.com>
2024-05-27 19:02:46 +0100

    Added additional video resources

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,5 +1,43 @@
 const projectsData = [
   {
+    title: 'Enhancing Memory Performance in Android TV Applications',
+    description: `This talk goes over developing a streaming video app for Android TV used by millions of users, where BAM faced a stringent challenge: keeping RAM usage under 200MB. Was RN up to the task? `,
+    imgSrc: 'http://img.youtube.com/vi/Poo58mamLYg/hqdefault.jpg',
+    href: 'https://www.youtube.com/watch?v=Poo58mamLYg',
+  },
+  {
+    title: 'TV Apps in React Native — Part II',
+    description: `QA - Gio and Mo talk about how to use React Native to develop TV apps. With Sr Dev Advocate @ Amazon & Mo Javad - Head of Mobile @ Theodo. From React Native London -- Nov 20, 2023.
+    `,
+    imgSrc: 'http://img.youtube.com/vi/Sx9Cd1hmJns/hqdefault.jpg',
+    href: 'https://www.youtube.com/watch?v=Sx9Cd1hmJns',
+  },
+  {
+    title: 'TV Apps in React Native — Part I',
+    description: `Gio and Mo talk about how to use React Native to develop TV apps. With Sr Dev Advocate @ Amazon & Mo Javad - Head of Mobile @ Theodo. From React Native London -- Nov 20, 2023.
+    `,
+    imgSrc: 'http://img.youtube.com/vi/CBZTX39n2yc/hqdefault.jpg',
+    href: 'https://www.youtube.com/watch?v=CBZTX39n2yc',
+  },
+  {
+    title: 'Bringing the Best of React to TV Navigation: A React-Like Solution for Remote Control',
+    description: `Have you ever wondered how Netflix navigates within a page with a remote? When you press an arrow button, how does it know which element to focus on?
+    We had to build this for our TV replay app (using RN) and made it the React way.
+    Our challenge was to build on top of the React tree our own tree containing the elements that are focusable with the remote.
+    Through trial and error, we developed a new declarative approach. We'll share the steps we took to synchronise the React tree with the spatial TV layout.
+    By the end of this talk, you'll gain a deeper understanding of the subtleties of React, learn how to simplify complicated logic with a declarative API and discover the non-trivial considerations involved in syncing the React tree with another one.    
+    `,
+    imgSrc: 'http://img.youtube.com/vi/Asn1TmCH2b0/hqdefault.jpg',
+    href: 'https://www.youtube.com/watch?v=Asn1TmCH2b0',
+  },
+  {
+    title: 'How to build TV apps from scratch | Expo',
+    description: `As a part of SDK 50, we are (for the first time) introducing the ability to build applications for Apple TV and Android TV that include Expo modules. In this livestream we demonstrate how to build a TV app from scratch and how to build a TV app with the Ignite CLI.
+    `,
+    imgSrc: 'http://img.youtube.com/vi/PUMBWeLVuiw/hqdefault.jpg',
+    href: 'https://www.youtube.com/watch?v=PUMBWeLVuiw',
+  },
+  {
     title: 'From Custom Hardware to Android TV and Beyond - React Native',
     description: `ROXi application is built to work on all platforms with the same codebase, and therefore requires our React Native app to support touch displays, pointing devices (HID) as well as TV remotes. The topic covers a general overview of how we achieved this and our plans to take it to more platforms.
     `,
